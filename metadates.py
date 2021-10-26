04d2ddf760005614aec85337f38d552c862e088c
Enric_Giner <enricg@gmail.com>
2021-10-26 22:26:29 +0200

    llista arxius de directori

--- metadates.py
+++ metadates.py
@@ -2,8 +2,26 @@ import sys
 import os
 import json
 import pdfx
+from pathlib import Path
 
+def recorreDirectori(directori):
+    files = Path(directori).glob('**/*.pdf')
+    for file in files:
+        recuperaMetadata(file)
 
+def recuperaMetadata(arxiu):
+    print(arxiu)
+    pdf = pdfx.PDFx(str(arxiu))
+    pdfjson=pdf.get_metadata()
+    #references_list = pdf.get_references()
+    references_dict = pdf.get_references_as_dict()
+    print(references_dict)
+    #if pdfjson[Title]:
+    #    print(pdfjson[Title])
+    #else:
+    #    print("No hi ha titol")
+    #    print(pdfjson)
+#
 #def renombrar(antic, nou):
 #    archivo = "/home/decodigo/Documentos/python/archivos/archivo.txt"
 #    nombre_nuevo = "/home/decodigo/Documentos/python/archivos/archivo_renombrado.txt"
@@ -13,11 +31,15 @@ import pdfx
 # Programa principal
 # ------------------------------------------------------------------------------
 
-if len(sys.argv) >= 2:
-    pdf = pdfx.PDFx(sys.argv[1])
-    pdfjson=pdf.get_metadata()
-    #print(pdf.get_metadata())
-    print(pdfjson["Title"])
-    #print(pdfjsonp["Title"])
-else:
-    print("Fa falta un paràmetre");
+#directori("/home/enric/Decargas/telegram/Aportes_informaticos")
+#if len(sys.argv) >= 2:
+#    pdf = pdfx.PDFx(sys.argv[1])
+#    pdfjson=pdf.get_metadata()
+#    #print(pdf.get_metadata())
+#    print(pdfjson["Title"])
+#    #print(pdfjsonp["Title"])
+#else:
+#    print("Fa falta un paràmetre")/home/enric/Descargas/telegram/Aportes_informaticos
+
+directori = "/home/enric/Descargas/telegram/Aportes_informaticos"
+recorreDirectori(directori)
